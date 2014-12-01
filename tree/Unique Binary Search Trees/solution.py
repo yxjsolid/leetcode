a8255a7b1a49a0ff2001da003be73fb5e66f73da
yxjsolid <yxjsolid@gmail.com>
2014-12-02 02:28:30 +0800

    update

--- tree/Unique Binary Search Trees/solution.py
+++ tree/Unique Binary Search Trees/solution.py
@@ -1,51 +1,91 @@
+class MY_NODE():
+    def __init__(self):
+        self.serial = []
+        self.cnt = 1
+
 class Solution:
     # @param root, a tree node
     # @return an integer
     def __init__(self):
         self.resultDict = {}
-        self.resultDict[1] =  [[1]]
-        self.resultDict[2] = [[1,2],[2,1],]
+        node1 = MY_NODE()
+        node1.serial = [1]
 
-    def serialAppend(self, serial, n):
-        lastVal = serial[0]
-        newList = []
+        self.resultDict[1] =  [node1]
+
+        node2 = MY_NODE()
+        node2.serial = [1,2]
+
+        node3 = MY_NODE()
+        node3.serial = [2,1]
+
+        self.resultDict[2] = [node2,node3,]
+        self.n = 1
+
+    def serialAppend(self, node, n):
+        serial = node.serial
+        newNodeList = []
 
         index = 0
-        newList.append([n] + serial)
-        newList.append(serial + [n])
 
+        newNode = MY_NODE()
+        newNode.serial = serial + [n]
+        newNodeList.append(newNode)
+        newNode.cnt = node.cnt
+
+        start = 0
+        for val in serial:
+            if val > serial[0]:
+                serial = serial[start-1:]
+                break
+            start += 1
+        #newList.append(serial + [n])
+
+
+        lastVal = serial[0]
         for val in serial:
             if val <= lastVal:
                 pass
             else:
-                new = serial[0:index:] + [n] + serial[index:]
-                newList.append(new)
+                newNode = MY_NODE()
+
+                newNode.serial = serial[0:index:] + [n] + serial[index:]
+                newNode.cnt = node.cnt
+                newNodeList.append(newNode)
                 lastVal = val
             index += 1
-        return newList
+
+        return newNodeList
 
     def gen(self, n, lastResult):
         newResult = []
-        for serial in lastResult:
-            newList = self.serialAppend(serial, n)
-            newResult += newList
+        newNode = MY_NODE()
+        newNode.serial = [n]
+        newResult += [newNode]
+        newNode.cnt = 0
+        for node in lastResult:
+            newNode.cnt += node.cnt
+            newNodeList = self.serialAppend(node, n)
+            newResult += newNodeList
+
         self.resultDict[n] = newResult
-        return len(self.resultDict[n])
+
 
     def numTrees(self, n):
 
         if n == 1:
             return 1
-        if n == 2:
-            return 2
 
-        start = 1
-        for i in xrange(n -1, 0, -1):
-            if self.resultDict.has_key(i):
-                start = i
-            break
-
-        for i in range(start + 1, n + 1):
+        for i in range(2, n + 1):
             self.gen(i, self.resultDict[i - 1])
 
-        return len(self.resultDict[n])
+
+
+
+        cnt = 0
+        for node in self.resultDict[n]:
+            print node.cnt, node.serial
+            cnt += node.cnt
+
+
+        return cnt
--- tree/Unique Binary Search Trees/test.py
+++ tree/Unique Binary Search Trees/test.py
@@ -8,15 +8,19 @@ from tree.treeCommon import *
 
 
 def test(n):
+
+
+
+
     tt = time.time()
     s = Solution()
-    #unique = s.numTrees(n)
-    tt = time.time()
-    for i in range(1, 13):
-        unique = s.numTrees(i)
-    print time.time() - tt
+    unique = s.numTrees(n)
+    #tt = time.time()
+    #for i in range(1, 13):
+    #    unique = s.numTrees(i)
+    #print time.time() - tt
+    return unique
 
-    print unique
 
     #print "unique", unique
 
@@ -26,8 +30,32 @@ if __name__ == "__main__":
 
     tt = time.time()
 
-    test(12)
+    #test(5)
 
-    # for i in range(1, 13):
-    #     test(i)
-    # print time.time() - tt
+    reslut = [1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012]
+
+
+    for i in range(12, 13):
+        r = test(i)
+        if r != reslut[i -1]:
+            print "n: ", i,
+            print "correct:", reslut[i -1],
+            print "error:", r
+
+    print time.time() - tt
+
+
+    """
+1 1
+2 2
+3 5
+4 14
+5 42
+6 132
+7 429
+8 1430
+9 4862
+10 16796
+11 58786
+12 208012
+        """
