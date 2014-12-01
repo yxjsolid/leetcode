cfd4e8fbd24fde06311169f069656edf31e61f94
Jerry Yang <yxjsolid@gmail.com>
2014-12-01 19:24:34 +0800

    update

--- tree/Unique Binary Search Trees/solution.py
+++ tree/Unique Binary Search Trees/solution.py
@@ -1,237 +1,51 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class BSTREE_GEN():
-    def __init__(self, serial, infoNodeList = None):
-        self.serial = serial
-        self.infoNodeList =  infoNodeList
-        self.root = None
-        self.gen()
-
-    def setInfo(self, val, level, pos):
-        if self.infoNodeList:
-            node = self.infoNodeList[val-1]
-            node.updatePos(level, pos)
-
-    def gen(self):
-        for val in self.serial:
-            if self.root is None:
-                self.root = TreeNode(val)
-                self.setInfo(val, 0, -1)
-            else:
-                self.insert(val)
-
-    def insert(self, val):
-        curNode = self.root
-        nextNode = None
-
-        level = 0
-        while True:
-            level += 1
-            if val > curNode.val:
-                nextNode = curNode.right
-                if nextNode is None:
-                    curNode.right = TreeNode(val)
-                    self.setInfo(val, level, 1)
-                    return
-                else:
-                    curNode = nextNode
-            else:
-                nextNode = curNode.left
-                if nextNode is None:
-                    curNode.left = TreeNode(val)
-                    self.setInfo(val, level, 0)
-                    return
-                else:
-                    curNode = nextNode
-
-
-
-
-def appendResult(orig, n):
-
-    if n == 1:
-        newList = [[1]]
-    else:
-        newList = []
-        for row in orig:
-            for i in range(len(row) + 1):
-                newRow = row[:]
-                newRow.insert(i, n)
-                newList.append(newRow)
-
-    #for e in newList:
-    #    print e
-    return newList
-
-def genTreeSerialList(n):
-    serialList = []
-    for i in range(1, n+1):
-        serialList = appendResult(serialList, i)
-        #print serialList
-
-
-    #for row in serialList:
-    #    print row
-    return serialList
-
-def buildBSTree(treeIn, infoNodeList):
-    tree = BSTREE_GEN(treeIn, infoNodeList)
-    return tree.root
-
-
-
-
-class CAT_NODE(TreeNode):
-    def __init__(self, val):
-        TreeNode.__init__(self, val)
-        self.val = val
-        self.serialDict = {}
-        self.level = 0
-        self.leafSerialList = []
-        self.leafCnt = 0
-
-    def getNode(self, val):
-        if self.serialDict.has_key(val):
-            catNode = self.serialDict[val]
-        else:
-            catNode = CAT_NODE(val)
-            self.serialDict[val] = catNode
-        return catNode
-
-    def handle(self, serialListIn):
-        # print "val:", self.val,
-        # print "handle:", serialListIn
-
-        if len(serialListIn) == 2:
-            a, b = serialListIn
-            if (a > self.val and b > self.val) or (a < self.val and b < self.val):
-                serialListIn.sort()
-            else:
-                pass
-
-            if serialListIn in self.leafSerialList:
-                pass
-            else:
-                # print "val:", self.val,
-                # print "append:", serialListIn
-                self.leafSerialList.append(serialListIn)
-                self.leafCnt += 1
-        else:
-            val = serialListIn.pop()
-            node = self.getNode(val)
-            node.handle(serialListIn)
-
-
-    def getLeafCnt(self):
-        cnt = 0
-        if len(self.serialDict) == 0:
-            pass
-        else:
-            cnt = 1
-            pass
-
-
-        for nodeyKey in self.serialDict:
-            cnt += self.serialDict[nodeyKey].getLeafCnt()
-        return cnt
-
-
-
-
-
-
-    def dump(self, level):
-        level += 1
-        if len(self.serialDict) == 0:
-            self.printLeaf()
-            print
-
-        for nodeyKey in self.serialDict:
-            #self.printPad(level)
-            self.printVal()
-            self.serialDict[nodeyKey].dump(level)
-            #print self.serialDict[nodeyKey]
-
-        return level
-
-    def printPad(self, level):
-        pad = "\t"* level
-        print
-        print pad,
-
-    def printVal(self):
-        if self.val:
-            print self.val,
-
-    def printLeaf(self):
-        for serial in self.leafSerialList:
-            print "leaf:", serial
-
-
-class CAT_ROOT(CAT_NODE):
-    def __init__(self, n):
-        CAT_NODE.__init__(self, 0)
-        for i in range(n):
-            self.getNode(i + 1)
-
-
-    def dump(self, n):
-        for key in self.serialDict:
-            node = self.serialDict[key]
-            print node.val
-            node.dump(1)
-
-
-
-
-
-    pass
-
 class Solution:
     # @param root, a tree node
     # @return an integer
     def __init__(self):
-        self.err = False
-        self.serial = []
-        self.validDict ={}
-        self.uniqueTree = []
+        self.resultDict = {}
+        self.resultDict[1] =  [[1]]
+        self.resultDict[2] = [[1,2],[2,1],]
 
-        self.infoNodeList = []
-        pass
+    def serialAppend(self, serial, n):
+        lastVal = serial[0]
+        newList = []
 
+        index = 0
+        newList.append([n] + serial)
+        newList.append(serial + [n])
 
+        for val in serial:
+            if val <= lastVal:
+                pass
+            else:
+                new = serial[0:index:] + [n] + serial[index:]
+                newList.append(new)
+                lastVal = val
+            index += 1
+        return newList
+
+    def gen(self, n, lastResult):
+        newResult = []
+        for serial in lastResult:
+            newList = self.serialAppend(serial, n)
+            newResult += newList
+        self.resultDict[n] = newResult
+        return len(self.resultDict[n])
 
     def numTrees(self, n):
-        serialList = genTreeSerialList(n)
 
-        root = CAT_ROOT(n)
-        for serial in serialList:
-            root.handle(serial)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
 
-        cnt = root.dump(0)
+        start = 1
+        for i in xrange(n -1, 0, -1):
+            if self.resultDict.has_key(i):
+                start = i
+            break
 
-        cnt = root.getLeafCnt()
+        for i in range(start + 1, n + 1):
+            self.gen(i, self.resultDict[i - 1])
 
-        print "cnt :", cnt
-
-        pass
-
-
-    def getSerial(self, root):
-        self.serial = []
-        if root is None:
-            return True
-        self.Loop(root)
-
-    def Loop(self, root):
-        self.serial.append(root.val)
-
-        if root.left:
-            self.Loop(root.left)
-
-        if root.right:
-            self.Loop(root.right)
+        return len(self.resultDict[n])
--- tree/Unique Binary Search Trees/test.py
+++ tree/Unique Binary Search Trees/test.py
@@ -3,111 +3,31 @@ import time
 from solution import *
 from tree.treeCommon import *
 
-class MyNode():
-    def __init__(self, val):
-        self.val = val
-        self.leftSerial = []
-        self.rightSerial = []
 
-
-
-
-def appendResult(orig, n):
-
-    if n == 1:
-        newList = [[1]]
-    else:
-        newList = []
-        for row in orig:
-            for i in range(len(row) + 1):
-                newRow = row[:]
-                newRow.insert(i, n)
-                newList.append(newRow)
-
-    #for e in newList:
-    #    print e
-    return newList
-
-def genTreeSerialList1(n):
-    serialList = []
-    for i in range(1, n+1):
-        serialList = appendResult(serialList, i)
-        #print serialList
-
-    # cnt = 0
-    # for row in serialList:
-    #     if row[0] == 2:
-    #         cnt +=1
-    #         print row
-    # print cnt
-    return serialList
-
-
-
-
-def handleSerial(val, serial):
-
-    length = len(serial)
-    if serial[0] != val:
-        return
-
-    if length%2:
-        end = length
-    else:
-        end = length - 1
-
-    #print "\nserial in:", serial
-    for i in range(1, end, 2):
-        a = serial[i]
-        b = serial[i+1]
-        if a > val and b > val or a < val and b < val:
-            pass
-        else:
-            ll = [a,b]
-            ll.sort()
-            serial[i] = ll[0]
-            serial[i+1] = ll[1]
-
-    #print "serial out:", serial
-    return serial
-
-
-def handleList(val, serialList):
-    result = []
-
-    for serial in serialList:
-        ll = handleSerial(val, serial)
-
-        if ll and ll not in result:
-            result.append(ll)
-
-    for l in result:
-        print l
-
-    #print serialList
 
 
 
 def test(n):
-
+    tt = time.time()
     s = Solution()
-    unique = s.numTrees(n)
+    #unique = s.numTrees(n)
+    tt = time.time()
+    for i in range(1, 13):
+        unique = s.numTrees(i)
+    print time.time() - tt
 
-    print "unique", unique
+    print unique
+
+    #print "unique", unique
 
 if __name__ == "__main__":
     treeIn = [3,1,5,0,2,4,6,"x","x","x",3]
 
-    val = 1
 
+    tt = time.time()
 
-    n = 4
-    serialList = genTreeSerialList1(n)
+    test(12)
 
-    for i in range(1, n + 1):
-
-        handleList(i, serialList)
-
-    #test(3)
-
-    #genTreeSerialList(9)
+    # for i in range(1, 13):
+    #     test(i)
+    # print time.time() - tt
